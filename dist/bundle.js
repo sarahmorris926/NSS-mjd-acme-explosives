9d145a257d175ebae6351539161444f21bd2abd8
Sarah Morris <sarah.morris9@gmail.com>
2018-01-04 15:14:51 -0600

    Added most recent code with formatter.js

--- dist/bundle.js
+++ dist/bundle.js
@@ -66,11 +66,15 @@ module.exports.formatData = (data) => {
   //  change "type" value from integer to string value, ie "fragmentation" 
 
   let revisedProds = data[2].map( (prod) => {
-    prod.category = "new thing";
-    prod.type = "blowy up thing";
+    for (let prop in prod) {
+      prod[prop].category = "new thing";
+      prod[prop].type = "blowy up thing";
+    }
+
+
     return prod;
   });
-
+  console.log('revised', revisedProds);
 };
 },{}],3:[function(require,module,exports){
 'use strict';
--- javascripts/formatter.js
+++ javascripts/formatter.js
@@ -12,9 +12,13 @@ module.exports.formatData = (data) => {
   //  change "type" value from integer to string value, ie "fragmentation" 
 
   let revisedProds = data[2].map( (prod) => {
-    prod.category = "new thing";
-    prod.type = "blowy up thing";
+    for (let prop in prod) {
+      prod[prop].category = "new thing";
+      prod[prop].type = "blowy up thing";
+    }
+
+
     return prod;
   });
-
+  console.log('revised', revisedProds);
 };
